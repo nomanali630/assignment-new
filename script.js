ab1d58d3d74c893cb68db005e64f8e83e15482b2
nomanali630 <61146280+nomanali630@users.noreply.github.com>
2020-11-30 10:35:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function shoes()
+let enter = document.getElementById("enter").value 
+let result = parseInt("result")
+
+for(let i = 1; i>= 1; i++){
+    if (i === 1)document("enter")
+    result.innerHTML = i +
+}
